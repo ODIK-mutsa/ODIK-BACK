e97c26b0ee69715dbd2b18e922b7a0897124e1a0
yuniyolli <yuniyolli@gmail.com>
2023-08-17 17:45:40 +0900

    add TourItemMapper - INSTANCE, toEntity, toDto, toListDto

--- src/main/java/com/micutne/odik/domain/tour/dto/TourItemMapper.java
+++ src/main/java/com/micutne/odik/domain/tour/dto/TourItemMapper.java
@@ -1,4 +1,18 @@
 package com.micutne.odik.domain.tour.dto;
 
+import com.micutne.odik.domain.tour.TourItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = "spring")
 public interface TourItemMapper {
+    TourItemMapper INSTANCE = Mappers.getMapper(TourItemMapper.class);
+
+    TourItem toEntity(TourItemRequest request);
+
+    TourItemResponse toDto(TourItem tourItem);
+
+    TourItemListResponse toListDto(TourItem tourItem);
 }
