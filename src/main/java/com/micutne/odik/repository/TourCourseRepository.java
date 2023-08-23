8845f9296e3c33a773064a516a0efa0ea951a3da
xxo <xxoxec@gmail.com>
2023-08-23 11:33:13 +0900

    add TourCourse - Repository

---
+++ src/main/java/com/micutne/odik/repository/TourCourseRepository.java
@@ -0,0 +1,8 @@
+package com.micutne.odik.repository;
+
+import com.micutne.odik.domain.tour.TourCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourCourseRepository extends JpaRepository<TourCourse, Long> {
+
+}
