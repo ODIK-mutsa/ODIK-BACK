4f6e0c2fcd0644d7767007408a2122054cefe37d
xxo <xxoxec@gmail.com>
2023-08-28 09:44:41 +0900

    add TourCourse - Repository 수정

--- src/main/java/com/micutne/odik/repository/TourCourseRepository.java
+++ src/main/java/com/micutne/odik/repository/TourCourseRepository.java
@@ -2,7 +2,13 @@ package com.micutne.odik.repository;
 
 import com.micutne.odik.domain.tour.TourCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TourCourseRepository extends JpaRepository<TourCourse, Long> {
 
+
 }
+
+
+
