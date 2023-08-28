87403fef28d461a531b01e265d3b56b82e56b96a
xxo <xxoxec@gmail.com>
2023-08-28 09:43:59 +0900

    add TourCourse - TourCourseItemList Entity

--- src/main/java/com/micutne/odik/domain/tour/TourCourseItemList.java
+++ src/main/java/com/micutne/odik/domain/tour/TourCourseItemList.java
@@ -1,4 +1,31 @@
 package com.micutne.odik.domain.tour;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
 public class TourCourseItemList {
+    @Id @GeneratedValue
+    private Long idx;
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "tour_course_idx")
+    private TourCourse tour_Course_idx;
+
+    @ManyToOne
+    @JoinColumn(name  = "tour_item_idx")
+    private TourItem tour_item_idx;
+    private String order;
+
+    private LocalDateTime date_create;
+
+
+    @ManyToOne
+    @JoinColumn(name = "tour_course_idx")
+    private TourCourse tourCourse;
+
 }
